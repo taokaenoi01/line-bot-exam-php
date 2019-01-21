663ad8eb2ad4d3b289e6a76eb253150eb7038459
taokaenoi01 <46810263+taokaenoi01@users.noreply.github.com>
2019-01-21 11:37:17 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 require "vendor/autoload.php";
-$access_token = 'xxxxx';
-$channelSecret = 'xxxxx';
+$access_token = 'k1+tnPGkJmLtwBucoSU+AQ/xKIaUFw+INxkscT9ZAZDtv8YueNd8IyXaraZG0zyxDlEno8pGOaZ1bti9wlcLrIWsYwh5P0m9ClZ9wv1ZUeffFIIYqlU6bMniKARmtHWlKPuBdA8LYe5stuktcQL8kwdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '64d2ac442ef2de0ec97fdbe2e469df4a';
 $idPush = 'xxxxxx'
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
