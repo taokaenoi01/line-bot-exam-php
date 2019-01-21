46adc8e68a4089b9b3cebb2d438da8a77d6a6692
taokaenoi01 <46810263+taokaenoi01@users.noreply.github.com>
2019-01-21 11:33:05 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+require "vendor/autoload.php";
+$access_token = 'xxxxx';
+$channelSecret = 'xxxxx';
+$idPush = 'xxxxxx'
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$response = $bot->pushMessage($idPush, $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
